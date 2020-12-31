8a4b611eb7743c612090e3f4d714fdb647e2f2cb
haerang <gofkdvjvl@naver.com>
2020-12-31 14:41:22 +0900

    feat: webpack 초기 설정 완료

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import 'App.css'
+const App = () => {
+ return <div><h1>react webpack boilerplate</h1>
+ <img src="./test.png" src="image"></img></div>
+};
+
+export default App;
