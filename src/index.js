8cba387e52e66e9aca94b50c81b131efa63aa64b
haerang <gofkdvjvl@naver.com>
2020-12-31 15:24:22 +0900

    feat:redux 설정 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "App";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from 'modules';
 
-ReactDOM.render( < App / > , document.querySelector("#root"));
+const store = createStore(rootReducer); // 스토어를 만듭니다.
+
+
+ReactDOM.render( <Provider store={store}>< App/></Provider> , document.querySelector("#root"));
